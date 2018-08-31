c0fc1611686b26e616c90e660e3b09c38a519ab6
Christopher Kaag <christopher.kaag@gmail.com>
2018-08-31 20:04:25 +0200

    also sewobe 70 will be pulled

--- query_sewobe.sh
+++ query_sewobe.sh
@@ -5,6 +5,7 @@ echo "SEWOBE Anfragen laufen"
 
 curl -X POST -F "USERNAME=$SEWOBEUSER" -F "PASSWORT=$SEWOBEPASSWORD" -F 'AUSWERTUNG_ID=70' $SEWOBEURL > "/go/src/app/testdata/sewobe27.json"
 curl -X POST -F "USERNAME=$SEWOBEUSER" -F "PASSWORT=$SEWOBEPASSWORD" -F 'AUSWERTUNG_ID=102' $SEWOBEURL > "/go/src/app/testdata/sewobe102.json"
+curl -X POST -F "USERNAME=$SEWOBEUSER" -F "PASSWORT=$SEWOBEPASSWORD" -F 'AUSWERTUNG_ID=70' $SEWOBEURL > "/go/src/app/testdata/sewobe70.json"
 curl -X POST -F "USERNAME=$SEWOBEUSER" -F "PASSWORT=$SEWOBEPASSWORD" -F 'AUSWERTUNG_ID=158' $SEWOBEURL > "/go/src/app/testdata/sewobe158.json"
 
 
